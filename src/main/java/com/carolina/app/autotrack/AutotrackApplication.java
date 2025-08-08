45a46de2d25a500847c74c80ea26589b8bf945ad
Carol Fagundes <carolinarftk@gmail.com>
2025-08-07 21:13:00 -0300

    first commit

---
+++ src/main/java/com/carolina/app/autotrack/AutotrackApplication.java
@@ -0,0 +1,13 @@
+package com.carolina.app.autotrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutotrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutotrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carolina/app/autotrack/AutotrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carolina.app.autotrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutotrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
